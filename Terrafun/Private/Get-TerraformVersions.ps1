8ccbc587ea40f06538cc5dc930dbf612e48f80cb
Douglas Brand-Williamson <doug.brand@amido.com>
2022-06-24 10:41:36 +0100

    fix: integrate Get-TerraformVersions with the hashicorp releases api (#4)
    * fix: integrate Get-TerraformVersions with the hashicorp releases api
    
    * fix: test failure on some powershell versions
    
    * fix: handle datetime conversion differences between pwsh versions
    
    * chore: revert to BeforeDiscovery
    
    * chore: remove unused mock

--- Terrafun/Private/Get-TerraformVersions.ps1
+++ Terrafun/Private/Get-TerraformVersions.ps1
@@ -1,19 +1,40 @@
 Function Get-TerraformVersions {
     [cmdletbinding()]
-    Param()
-    
-    $ReleaseUrl = "https://releases.hashicorp.com/terraform"
-    $Pattern = '^\/terraform\/[0-9]+\.[0-9]+\.[0-9]+$'
-    Write-Verbose "getting terraform versions..."
-    $Response = Invoke-WebRequest -UseBasicParsing -Uri $ReleaseUrl
-    $Versions = $Response.Links.href | Where-Object { $_ -like "/terraform*" }
+    Param(
+    )
+
+    # api reference
+    # https://releases.hashicorp.com/docs/api/v1/#operation/listReleasesV1
 
     $Releases = @()
-    foreach ($Version in $Versions){
-        if ($Version -match $Pattern){
-            $Releases += $Version -replace "/terraform" -replace "/"
+    $Limit = 20
+    $Uri = "https://api.releases.hashicorp.com/v1/releases/terraform?limit={0}" -f $Limit
+
+    do{
+
+        $Response = Invoke-RestMethod -Method Get -Uri $Uri -ContentType "application/json"
+        if ($Response.count -eq 0){
+            Break
         }
+        $Releases += $Response
+
+        # get the date of the oldest release - used for pagination
+        # powershell automatically converts the timestamp strings to [datetime] objects
+        # so the string needs to be formatted correctly when being used for pagination
+        # this behaviour various across different powershell versions
+        # see # https://github.com/PowerShell/PowerShell/issues/13592
+        $OldestRelease = $Response.timestamp_created | Sort-Object {[datetime]$_} | Select-Object -First 1
+        $AfterTimestamp = (Get-Date -Date $OldestRelease).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssK")
+        $Uri = "https://api.releases.hashicorp.com/v1/releases/terraform?limit={0}&after={1}" -f $Limit, $AfterTimestamp
+        Write-Debug -Message $Uri
+        Write-Debug -Message ("AfterTimestamp={0}, page={1}, total={2}" -F $AfterTimestamp, $Response.count, $Releases.count)
+
     }
+    until($Response.count -eq 0)
+
+    $ReleaseVersions = $Releases | Where-Object {$_.is_prerelease -ne $True}
+    $ReleaseVersions = $ReleaseVersions | Select-Object -ExpandProperty version
+
+    Return [string[]]($ReleaseVersions | Sort-Object {[version] $_})
     
-    Return [string[]]($Releases | Sort-Object {[version] $_})
 }
